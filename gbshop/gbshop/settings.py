9b9b0fa0f911284a905ac58cc6a6d601aabf9b9a
bearlexxx <alexey.d84@gmail.com>
2022-02-01 10:24:50 +0300

    lesson_2.5 add change for prod

--- gbshop/gbshop/settings.py
+++ gbshop/gbshop/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -93,10 +94,24 @@ WSGI_APPLICATION = 'gbshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gbshop',
+        'USER': 'postgres',
+        # 'PASSWORD': 'password',
+        # 'HOST': 'db.example.com',
+        # 'PORT': '5432',
+        # 'OPTIONS': {
+        #     'sslmode': 'require'
+        # }
     }
 }
 
@@ -136,9 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRES = (
-    os.path.join(BASE_DIR, "gbshop", "static")
-)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# STATICFILES_DIRES = (
+#     os.path.join(BASE_DIR, "gbshop", "static")
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
